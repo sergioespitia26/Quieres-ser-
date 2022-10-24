0b03583ec1095d25ebd460b70d2584976065f14e
sergioespitia26 <68637660+sergioespitia26@users.noreply.github.com>
2022-10-23 19:19:27 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const yesBtn = document.querySelector('#yesBtn');
 
 yesBtn.addEventListener('click',function () {
-     alert('  👌 👈 😈')
+     alert('TE AMO...')
 });
 
 const noBtn = document.querySelector('#noBtn');
